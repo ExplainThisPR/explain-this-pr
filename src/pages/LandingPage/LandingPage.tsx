eb6ef315ca18b8580e375ff0f7cc324bf2bbd36c
Verdi Kapuku <v@askmora.io>
2023-03-20 11:34:05 -0500

    Add more meat to the bone
    Style it some more. Added a bit more content and some logic too

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -4,12 +4,20 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGemoji from 'remark-gemoji';
 import remarkGfm from 'remark-gfm';
+import { doc, onSnapshot } from 'firebase/firestore';
 import './LandingPage.css';
+import { db } from '../../firebase';
 
+type PublicData = {
+  last_run_at: string;
+  loc_analyzed: number;
+  runs: number;
+};
 function LandingPage() {
   const [diff, setDiff] = React.useState('');
   const [result, setResult] = React.useState('');
   const [loading, setLoading] = React.useState(false);
+  const [stats, setStats] = React.useState<PublicData | null>(null);
   // Call the Firebase Function with diff_body as the body
   const handleExplain = async () => {
     try {
@@ -29,18 +37,58 @@ function LandingPage() {
       message.error('Your request failed. Please try again');
     }
   };
+  React.useEffect(() => {
+    const unsub = onSnapshot(doc(db, 'AdminDashboard', 'public'), (doc) => {
+      if (doc.exists()) {
+        const data = doc.data() as PublicData;
+        setStats(data);
+        console.log(data);
+      }
+    });
+    return () => {
+      unsub();
+    };
+  }, []);
+  const analyzedCode = React.useMemo(() => {
+    const value = stats?.loc_analyzed || 1000;
+    return value.toLocaleString();
+  }, [stats?.loc_analyzed]);
 
   return (
-    <div className="LandingPage">
+    <div className="page-container">
       <Typography.Title>ExplainThisPR</Typography.Title>
-      <Typography.Paragraph>
-        1. Use the Github CLI to run this command:
-      </Typography.Paragraph>
+      <Typography.Title level={4}>
+        1. Use the{' '}
+        <a
+          href="https://github.com/cli/cli#installation"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github CLI
+        </a>{' '}
+        to run this command
+      </Typography.Title>
+      <Row justify="center">
+        <Col span={8} className="sh-code">
+          <Typography.Text copyable className="sh-code-text">
+            gh api \
+            <br /> -H "Accept: application/vnd.github+json" \
+            <br /> -H "X-GitHub-Api-Version: 2022-11-28" \
+            <br /> /repos/[OWNER]/[REPO]/pulls/[PULL]/files \
+            <br /> {'>'} output.json
+          </Typography.Text>
+        </Col>
+      </Row>
+      <br />
+      <Typography.Title level={4}>
+        2. Copy the contents of the output.json file and paste it below
+      </Typography.Title>
+      <br />
       <Row justify="center">
         <Col span={18}>
           <Input.TextArea
             className="textarea font-mono"
-            placeholder="Paste your PR diff here"
+            placeholder="Paste the response from the pull/{id}/files API call here"
             autoSize={{ minRows: 10, maxRows: 20 }}
             onChange={(e) => setDiff(e.target.value)}
             value={diff}
@@ -49,17 +97,61 @@ function LandingPage() {
       </Row>
       <br />
       <Row justify="center">
-        <Button size="large" onClick={handleExplain} loading={loading}>
-          Explain this PR
-        </Button>
+        <Col span={5}>
+          <Button
+            size="large"
+            onClick={handleExplain}
+            loading={loading}
+            style={{ width: '100%' }}
+          >
+            Get to work
+          </Button>
+        </Col>
       </Row>
       <Divider />
-      <ReactMarkdown
-        className="markdown-container font-mono"
-        remarkPlugins={[remarkGfm, remarkGemoji]}
-      >
-        {result}
-      </ReactMarkdown>
+      {result ? (
+        <ReactMarkdown
+          className="markdown-container font-mono"
+          remarkPlugins={[remarkGfm, remarkGemoji]}
+          children={result}
+        />
+      ) : (
+        <div className="markdown-container font-mono">
+          <Typography.Text className="font-mono">
+            Well, what are you waiting for?
+          </Typography.Text>
+        </div>
+      )}
+      <br />
+      <Typography.Title level={3}>
+        We have already processed over {analyzedCode} lines of code and
+        counting!
+      </Typography.Title>
+      <br />
+      <Col span={24}>
+        <a
+          href="https://github.com/frenchmajesty"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="social-icon"
+            src="./icons/github.svg"
+            alt="Github logo"
+          />
+        </a>
+        <a
+          href="https://twitter.com/frenchmajesty"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="social-icon"
+            src="./icons/twitter.svg"
+            alt="Twitter logo"
+          />
+        </a>
+      </Col>
     </div>
   );
 }
