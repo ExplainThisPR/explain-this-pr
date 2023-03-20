e46c9b62c710ac06c326c5b18c1b5707560fc2ff
Verdi Kapuku <v@askmora.io>
2023-03-20 08:12:20 -0500

    Setup the Router

---
+++ src/firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBphO--zzEFl5z9BIKmtLpnmoTR-0vXNOo',
+  authDomain: 'explain-this-pr.firebaseapp.com',
+  projectId: 'explain-this-pr',
+  storageBucket: 'explain-this-pr.appspot.com',
+  messagingSenderId: '365925585278',
+  appId: '1:365925585278:web:19073846dfacc129ec232f',
+  measurementId: 'G-FB7XCWHRFH',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <header className="App-header">
+        <p>This is the Landing Page</p>
+      </header>
+    </div>
+  );
+}
+
+export default LandingPage;
