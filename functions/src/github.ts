ba0a1313ffdbcf9eb6827c7764f2477bcb972a96
Verdi Kapuku <v@askmora.io>
2023-03-20 06:30:28 -0500

    Bring over the Github webhook logic

---
+++ functions/src/github.ts
@@ -0,0 +1,115 @@
+import { config, logger } from 'firebase-functions';
+import * as crypto from 'crypto';
+import { RestEndpointMethodTypes } from '@octokit/plugin-rest-endpoint-methods/dist-types';
+import { App } from 'octokit';
+import { extensions } from './static_data';
+
+const appId = config().github.app_id;
+const privateKey = config().github.private_key;
+const gh = new App({ appId, privateKey });
+
+type PullRequestFiles =
+  RestEndpointMethodTypes['pulls']['listFiles']['response']['data'];
+
+export default class Github {
+  static createInstance(installationId: number) {
+    return gh.getInstallationOctokit(installationId);
+  }
+
+  static verifySignature(payloadBody: string, headerSignature: string): void {
+    const SECRET_TOKEN = config().github.webhook_secret;
+    const algo = 'sha256';
+    const encrypted = crypto
+      .createHmac(algo, SECRET_TOKEN)
+      .update(payloadBody)
+      .digest('hex');
+    const signature = `${algo}=${encrypted}`;
+
+    if (
+      !headerSignature ||
+      !crypto.timingSafeEqual(
+        Buffer.from(signature),
+        Buffer.from(headerSignature),
+      )
+    ) {
+      throw new Error("Signatures didn't match!");
+    }
+  }
+
+  /**
+   * Filter out files that we don't want to summarize
+   * @param files The list of files to filter
+   * @returns
+   */
+  static filterOutFiltersToAnalyze(files: PullRequestFiles) {
+    let result = files.filter((file) => {
+      const filename = file.filename.toLowerCase();
+      if (
+        filename.includes('test') ||
+        filename.includes('spec') ||
+        filename.includes('mock') ||
+        filename.includes('fixture') ||
+        filename.includes('package.json') ||
+        filename.includes('package-lock.json')
+      ) {
+        return false;
+      }
+
+      if (filename.includes('icon')) {
+        return false;
+      }
+
+      if (file.changes < 1) {
+        return false;
+      }
+
+      const fileParts = filename.split('.');
+      const extension = '.' + fileParts[fileParts.length - 1];
+
+      return extensions.includes(extension);
+    });
+
+    // Update the content `patch` of the files that were deleted
+    result = result.map((file) => {
+      if (file.status === 'removed') {
+        file.patch = 'File deleted';
+      }
+      return file;
+    });
+
+    logger.info(`Files before filter: ${files.length}`);
+    logger.info(`Files after filter: ${result.length}`);
+    return result;
+  }
+
+  /**
+   * Break the files into chunks of 9k or less characters to
+   * stay within the GPT tokens limit
+   * @param files The list of files to break into chunks
+   * @returns
+   */
+  static breakFilesIntoChunks(files: PullRequestFiles) {
+    const result: { filename: string; content: string }[][] = [];
+    let index = 0;
+    let totalChar = 0;
+    const limit = 9000;
+
+    files.forEach((file) => {
+      const filename = file.filename;
+      const content = file.patch || '';
+      if (totalChar + content.length > limit) {
+        index++;
+        totalChar = 0;
+      }
+
+      if (!result[index]) {
+        result[index] = [];
+      } else {
+        result[index].push({ filename, content });
+        totalChar += content.length;
+      }
+    });
+
+    return result;
+  }
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,125 @@
-import * as functions from "firebase-functions";
+import { https, logger, config } from 'firebase-functions';
+import Github from './github';
+import { PullRequestLabeledEvent } from '@octokit/webhooks-types';
+import { Configuration, OpenAIApi } from 'openai';
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+const apiKey = config().openai.api_key;
+const configuration = new Configuration({
+  apiKey,
 });
+const OpenAI = new OpenAIApi(configuration);
+
+export const githubWebhook = https.onRequest(async (request, response) => {
+  logger.log({ action: request.body.action });
+
+  const signature = request.header('x-hub-signature-256');
+  Github.verifySignature(JSON.stringify(request.body()), signature || '');
+
+  let installationId = 0;
+  let repoName = '';
+  let repoOwner = '';
+  let pullNumber = 0;
+
+  if (request.body().action === 'labeled') {
+    const payload = request.body() as PullRequestLabeledEvent;
+    const label = payload.label.name.toLowerCase();
+    if (label !== 'explainthispr') {
+      response.send({
+        message: 'Nothing for me to do.',
+      });
+      return;
+    }
+
+    installationId = payload.installation?.id || 0;
+    repoName = payload.repository.name;
+    repoOwner = payload.repository.owner.login;
+    pullNumber = payload.pull_request.number;
+  } else {
+    response.send({
+      message: 'Nothing for me to do.',
+    });
+    return;
+  }
+
+  const octokit = await Github.createInstance(installationId);
+  const data = await octokit.rest.pulls.listFiles({
+    owner: repoOwner,
+    repo: repoName,
+    pull_number: pullNumber,
+  });
+
+  const filestoAnalyze = Github.filterOutFiltersToAnalyze(data.data);
+  const filenames = filestoAnalyze.map((file) => file.filename);
+  const chunks = Github.breakFilesIntoChunks(filestoAnalyze);
+  logger.info(`Number of chunks to send: ${chunks.length}`);
+  logger.info(filenames);
+
+  const responses = await Promise.all(
+    chunks.map(async (chunk) => {
+      const combined = chunk.reduce((acc, file) => acc + file.content, '');
+      const gpt = await getSummaryForPR(combined);
+      const message = gpt?.choices[0].message?.content;
+      return message || '';
+    }),
+  );
+  logger.info(responses);
+
+  if (request.body.action === 'labeled') {
+    logger.info('Adding a comment to the PR..');
+    try {
+      await octokit.rest.issues.createComment({
+        owner: repoOwner,
+        repo: repoName,
+        issue_number: pullNumber,
+        body: 'I am working on it!',
+      });
+    } catch (e: any) {
+      const error = e?.response?.data || e;
+      console.error('Failed to create comment', error);
+    }
+  }
+
+  response.send({
+    message: 'Well, done!',
+  });
+});
+
+async function getSummaryForPR(content: string) {
+  try {
+    const prompt = `
+    You are an assistant designed to help developer teams review the PRs. Every time a PR is opened, I will send you a code file with the before and the after.
+    I want you to compare the two and give me an overview of the things that were changed in the code in a bullet list format.
+    I want your comments to be focused on architectural changes that affect the app.
+    The format of your response should be in Markdown. Here is an example of a response.
+    Limit how many import statements you have per file to 3 max.
+    Please do not repeat yourself. If you already mention a technology like \`useFonts\` do not mention it again for that file.
+    Please use the full filename. For example, \`src/components/Button.tsx\` is better than \`Button.tsx\`.
+    Please be concise and do not talk repeatedly about the same change. If you have already mentioned how PDF data generation works for example, do not discuss again it.
+    Only focus on the most impactful functional changes. The is 4 bullet points. For each additional bullet point, divide the total \`patch\` length by 1500 to determine how many more bullet points you can add for this file.
+    ## src/App.tsx
+      - Moved the routing logic to a standalone \`Router\` component
+    `;
+
+    const { data } = await OpenAI.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      max_tokens: 400,
+      temperature: 0.3,
+      frequency_penalty: 0.2,
+      presence_penalty: 0.3,
+      top_p: 1,
+      messages: [
+        { role: 'system', content: prompt },
+        { role: 'user', content },
+      ],
+    });
+    console.log('GPT Response: ', {
+      usage: data.usage,
+      content: data.choices[0],
+    });
+    return data;
+  } catch (err: any) {
+    const error = err?.response?.data || err;
+    console.error('Failed to make GPT request', error);
+    return null;
+  }
+}
